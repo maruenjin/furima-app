d05a37556ad02da7ddd320790ad9f2dd0cc41110
maruyu <goldenleaf3310@gmail.com>
2025-09-28 23:33:17 +0900

    seed: use ProductSpecSeeder only; remove random factories

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -3,17 +3,19 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
+    public function run(): void
     {
-        \App\Models\User::factory(10)->create();  
-        \App\Models\Product::factory(30)->create();
+        
+        
+
+        
+        $this->call([
+            ProductSpecSeeder::class,
+        ]);
     }
 }
+
